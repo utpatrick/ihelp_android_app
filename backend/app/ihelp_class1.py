2651c41d8fc1393e9f7e4cf7795662d549f7d00c
utpatrick <lordbruce727@gmail.com>
2017-11-15 13:28:10 -0600

    adding basic functions

---
+++ backend/app/ihelp_class1.py
@@ -0,0 +1,37 @@
+# [START imports]
+import os
+import model
+import json
+import time
+
+import jinja2
+import webapp2
+
+from google.appengine.api import users
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext import ndb
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+IMAGE_COUNT = 16
+
+
+class GetAllStreams(webapp2.RequestHandler):
+    def get(self):
+        orig_streams = model.get_all_stream()
+        streams = sorted(orig_streams, key=lambda x: x.last_update, reverse=True)
+        response_content = [{'stream_name': s.stream_name, 'cover_image': s.cover_image} for s in streams]
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(response_content))
+
+
+# [START app]
+app = webapp2.WSGIApplication([
+    ('/android/view_all_streams', GetAllStreams)
+], debug=True)
+# [END app]
