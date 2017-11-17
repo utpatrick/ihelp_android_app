8dfb3be311fc254d078090c0f04299d6753c02c7
Yuankai Yue <yueyuankai@gmail.com>
2017-11-17 11:47:49 -0600

    add view task empty activity

---
+++ iHelp/app/src/main/java/com/utexas/ee382v/ihelp/ViewTask.java
@@ -0,0 +1,13 @@
+package com.utexas.ee382v.ihelp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_task);
+    }
+}
