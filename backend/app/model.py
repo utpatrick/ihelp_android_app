eb1b3fb97b1bbf431f29ae9cc7cf7191bfc1e9c5
Yuankai Yue <yueyuankai@gmail.com>
2017-12-10 17:40:17 -0600

    edit set helpee and helper during task creation

--- backend/app/model.py
+++ backend/app/model.py
@@ -15,7 +15,7 @@ class Task(ndb.Model):
     owner_email = ndb.StringProperty()
     helper = ndb.StringProperty()
     #helpee is the one who is helped by helper
-    helpee = ndb.StringProperty
+    helpee = ndb.StringProperty()
 
     title = ndb.StringProperty()
     description = ndb.StringProperty()
@@ -35,11 +35,17 @@ class Task(ndb.Model):
 
 def post_a_task(task_title, task_category, task_type, task_detail,
                 task_location, desitination_location, task_status,
-                task_onwer, extra_credit, expiration_time = 3600):
-    new_task = Task(title=task_title, category=task_category, type=task_type, description=task_detail,
-                    task_location=task_location, final_dest=desitination_location, status=task_status,
-                    owner_email=task_onwer, credit=10)
-    new_task.put()
+                task_owner, extra_credit, expiration_time = 3600):
+    if task_type == 'provide_help':
+        new_task = Task(title=task_title, helper=task_owner, category=task_category, type=task_type, description=task_detail,
+                        task_location=task_location, final_dest=desitination_location, status=task_status,
+                        owner_email=task_owner, credit=10)
+        new_task.put()
+    else:
+        new_task = Task(title=task_title, helpee=task_owner, category=task_category, type=task_type, description=task_detail,
+                        task_location=task_location, final_dest=desitination_location, status=task_status,
+                        owner_email=task_owner, credit=10)
+        new_task.put()
     return 0
 
 
