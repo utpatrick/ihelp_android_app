c437b602616f2cf3e350cb6064f81d1503a3a8c9
SHEN_XIAOCHENG <845876310@qq.com>
2017-12-10 23:04:18 -0600

    pull

--- iHelp/app/src/main/java/com/utexas/ee382v/ihelp/MainActivity.java
+++ iHelp/app/src/main/java/com/utexas/ee382v/ihelp/MainActivity.java
@@ -1,27 +1,17 @@
 package com.utexas.ee382v.ihelp;
 
-import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.VideoView;
 
 import com.android.volley.NetworkResponse;
@@ -37,10 +27,8 @@ import com.applozic.mobicomkit.api.account.register.RegistrationResponse;
 import com.applozic.mobicomkit.api.account.user.MobiComUserPreference;
 import com.applozic.mobicomkit.api.account.user.PushNotificationTask;
 import com.applozic.mobicomkit.api.account.user.User;
-import com.applozic.mobicomkit.api.account.user.UserClientService;
 import com.applozic.mobicomkit.api.account.user.UserLoginTask;
 import com.applozic.mobicomkit.api.account.user.UserLogoutTask;
-import com.applozic.mobicomkit.feed.ApiResponse;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -64,7 +52,6 @@ import com.google.android.gms.common.api.Status;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -88,8 +75,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static String user_display_name;
     private boolean signinStatus;
     private Context mContext;
-//    private static final String BACKEND_ENDPOINT = "https://firebase-ihelp.appspot.com";
-    private static final String BACKEND_ENDPOINT = "http://10.0.2.2:8080";
+    private static final String BACKEND_ENDPOINT = "https://firebase-ihelp.appspot.com";
+    //private static final String BACKEND_ENDPOINT = "http://10.0.2.2:8888";
     private VideoView mVideoView;
 
     @Override
@@ -185,27 +172,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mVideoView = (VideoView)findViewById(R.id.bgvideoview);
-        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.bg);
-        mVideoView.setVideoURI(uri);
-        mVideoView.start();
-
-        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.setLooping(true);
-
-            }
-        });
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
+    
 
     public static String getUserName() {
         if(gname != null) {
